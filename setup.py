fc54ad96a96e5d1639e5315f5fb8b9dcc01e54e5
Jeff Terrace <jterrace@cs.princeton.edu>
2011-03-21 13:45:39 -0400

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import os
+
+setup (name = "Meshtool",
+            version = "0.1",
+            description = "Tool for manipulating collada meshes",
+            author = "Jeff Terrace",
+            packages = ['meshtool']
+            )
