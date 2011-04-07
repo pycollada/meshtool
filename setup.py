e1a37b4fb2f9cac66d4bc21f2d4307487401053a
Jeff Terrace <jterrace@gmail.com>
2011-04-07 19:23:54 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = "Meshtool",
+    name = "meshtool",
     version = "0.1",
     description = "Tool for manipulating collada meshes",
-    author = "Jeff Terrace",
+    author = "Jeff Terrace and contributors",
+    author_email = 'jterrace@gmail.com',
+    platforms=["any"],
+    license="BSD",
+    install_requires=['pycollada>=0.1', 'PIL'],
+    url = "https://github.com/pycollada/meshtool",
+    entry_points = {
+        'console_scripts':[
+            'meshtool = meshtool.__main__:main'
+        ]
+    },
     packages = find_packages()
 )
