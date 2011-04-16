a756457fc8637bced9910daf29afd1f27cc8bf20
Jeff Terrace <jterrace@gmail.com>
2011-04-15 20:25:11 -0400

    Bumping to 0.1.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name = "meshtool",
-    version = "0.1.1",
+    version = "0.1.2",
     description = "Tool for manipulating collada meshes",
     author = "Jeff Terrace and contributors",
     author_email = 'jterrace@gmail.com',
     platforms=["any"],
     license="BSD",
-    install_requires=['pycollada>=0.2', 'PIL', 'argparse'],
+    install_requires=['pycollada>=0.2.1', 'PIL', 'argparse'],
     url = "https://github.com/pycollada/meshtool",
     entry_points = {
         'console_scripts':[
