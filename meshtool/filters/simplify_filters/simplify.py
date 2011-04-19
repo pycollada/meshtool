736d696a443182f8e6e536ac56a1a649c8deb4cd
Eddy Ferreira <eferreir@princeton.edu>
2011-04-18 23:50:54 -0400

    Made simplify filter not take a filename and made it only accept a triangulated mesh

--- meshtool/filters/simplify_filters/simplify.py
+++ meshtool/filters/simplify_filters/simplify.py
@@ -7,15 +7,13 @@ from .mesh_simplification import MeshSimplification
 import collada
 
 def FilterGenerator():
-    class ColladaSimplifyFilter(SaveFilter):
+    class ColladaSimplifyFilter(OpFilter):
         def __init__(self):
             super(ColladaSimplifyFilter, self).__init__('simplify', 'Simplifies a collada file')
-        def apply(self, mesh, filename):
-            if os.path.exists(filename):
-                raise FilterException("specified filename already exists")
+        def apply(self, mesh):
             prim = mesh.geometries[0].primitives[0]
             if not isinstance(prim, collada.triangleset.TriangleSet):
-                prim = prim.triangleset()
+                raise FilterException("Must triangulate before simplifying")
             s = MeshSimplification(prim.vertex, prim.vertex_index)
             for i in range(len(prim.vertex)*4/5):
                 s.contractOnce()
@@ -27,6 +25,5 @@ def FilterGenerator():
             indices = numpy.array(s.triangles).flatten()
             new_prim = mesh.geometries[0].createTriangleSet(indices, il, "somematerial")
             mesh.geometries[0].primitives[0] = new_prim
-            mesh.write(filename)
             return mesh
     return ColladaSimplifyFilter()
