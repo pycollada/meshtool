d059a45f6c58f691bb1756531545dc2a9e7773d5
Eddy Ferreira <eferreir@princeton.edu>
2011-04-24 14:40:16 -0400

    Progressive mesh generation (not yet tested). Also, make simplification actually prune the collada file of all source list data that has been simpified away.

--- meshtool/filters/simplify_filters/mesh_simplification.py
+++ meshtool/filters/simplify_filters/mesh_simplification.py
@@ -25,29 +25,50 @@ def quadricForTriangle(triangle, progress):
 def evalQuadric(A, b, c, pt):
     return numpy.dot(pt,numpy.inner(A,pt)) + 2*numpy.dot(b,pt) + c
 
+class ContractionRecord:
+    # Contains:
+    # source - index of vertex that is deleted
+    # target - index of vertex that is contracted to
+    # deleted_triangles - list of deleted triangles (by index)
+    # deleted_triangles_opp_v - list of opposite vertices from contracted edge
+    #                           in deleted triangles (by index)
+    # deleted_triangles_perm - for each deleted triangle, a permutation of
+    #                          (0,1,2), indicating which of the three corners
+    #                          is the source, the target, and the opposite corner.
+    # changed_triangles - list of changed triangles (by index)
+    def __init__(self, source, target, deleted_triangles,
+                 deleted_triangles_opp_v, deleted_triangles_perm,
+                 changed_triangles):
+        self.source = source
+        self.target = target
+        self.deleted_triangles = deleted_triangles
+        self.deleted_triangles_opp_v = deleted_triangles_opp_v
+        self.deleted_triangles_perm = deleted_triangles_perm
+        self.changed_triangles = changed_triangles
+
 class MeshSimplification:
 
-    # Instance variables:
-    #
-    # vertices: array of vertices
-    #
-    # triangles: array of triangles (each triangle:
-    # 3 indices into vertices array)
-    #
-    # NO ____  adj: array of lists of triangle indices that a vertex appears on (parallel
-    # to vertices array)
-    #
-    # quadrics: quadric of each vertex (parallel to vertices array)
-    #
-    # contractions: array of lists of contractions that a vertex appears in
-
-    def __init__(self, vertices, triangles, corner_attributes=[]):
+    def __init__(self, vertices, triangles, attributes=[],
+                 attribute_sources=[]):
         print "Copying..."
-        self.vertices = [copy.copy(vertex) for vertex in vertices]
+        self.original_vertices = vertices
+        self.original_triangles = triangles
+        self.original_attributes = attributes
+        self.original_attribute_sources = attribute_sources
+        self.vertices = [vertex for vertex in vertices]
+        self.vertex_indices = range(len(vertices))
+        self.history = []
         self.triangles = [copy.copy(triangle) for triangle in triangles]
-        self.corner_attributes = [[copy.copy(triangle) for triangle in attr_list]
-                                  for attr_list in corner_attributes]
-        self.adj = [{} for i in range(len(vertices))]
+        self.triangle_indices = range(len(triangles))
+        self.attributes = [[copy.copy(triangle) for triangle in attr_list]
+                           for attr_list in attributes]
+        self.attribute_sources = [[attr for attr in attr_list]
+                                  for attr_list in attribute_sources]
+        self.attribute_indices = [range(len(attr_list))
+                                  for attr_list in attribute_sources]
+        self.adj = [{} for v in vertices]
+        self.attribute_adj = [[{} for attr in attr_list]
+                              for attr_list in attribute_sources]
         self.adj_edge = {}
         print "Building simplex..."
         progress = ProgressPrinter(len(triangles))
@@ -63,6 +84,12 @@ class MeshSimplification:
             self.adj[a][i] = 0
             self.adj[b][i] = 1
             self.adj[c][i] = 2
+            for j in range(len(self.attribute_adj)):
+                (a,b,c) = attributes[j][i]
+                self.attribute_adj[j][a][i] = True
+                self.attribute_adj[j][b][i] = True
+                self.attribute_adj[j][c][i] = True
+
         print "Generating triangle quadrics..."
         progress = ProgressPrinter(len(triangles))
         self.tri_quadrics = [quadricForTriangle(triangle, progress)
@@ -84,6 +111,58 @@ class MeshSimplification:
             (a,b,c) = triangles[i]
             for x in [(a,b),(a,c),(b,c)]:
                 self.genContraction(x[0], x[1])
+
+    def generatePM(self):
+        pm = []
+        new_v_indices = [-1 for v in self.original_vertices]
+        num_v = len(self.vertex_indices)
+        for i in range(num_v):
+            new_v_indices[self.vertex_indices[i]] = i
+        new_t_indices = [-1 for t in self.original_triangles]
+        num_t = len(self.triangle_indices)
+        for i in range(num_t):
+            new_t_indices[self.triangle_indices[i]] = i
+        new_a_indices = [[-1 for a in attr_list]
+                         for attr_list in self.original_attribute_sources]
+        num_a = [len(attr_list) for attr_list in self.attribute_indices]
+        for attr_id in range(len(self.attribute_indices)):
+            for i in range(len(self.attribute_indices[attr_id])):
+                new_a_indices[attr_id][self.attribute_indices[attr_id][i]] = i
+        for i in range(len(self.history) - 1, -1, -1):
+            rec = self.history[i]
+            coords = self.original_vertices[rec.source]
+            split_index = new_v_indices[rec.target]
+            if split_index == -1: raise Exception
+            changed_triangles = [new_t_indices[i]
+                                 for i in rec.changed_triangles]
+            new_triangles_opp_v = [new_v_indices[i]
+                                   for i in rec.deleted_triangles_opp_v]
+            new_triangles_attr = [[] for x in self.original_attributes]
+            for j in range(len(rec.deleted_triangles_perm)):
+                i1, i2, i3 = rec.deleted_triangles_perm[j]
+                tri_index = rec.deleted_triangles[j]
+                for k in range(len(new_triangles_attr)):
+                    attr_orig = self.original_attributes[k][tri_index]
+                    attr_cur = []
+                    for ind in (i1, i2, i3):
+                        a = attr_orig[ind]
+                        cur_i = new_a_indices[k][a]
+                        if cur_i == -1:
+                            attr_cur.append(
+                                self.original_attribute_sources[k][a])
+                            new_a_indices[k][a] = num_a[k]
+                            num_a[k] += 1
+                        else:
+                            attr_cur.append(cur_i)
+                    new_triangles_attr[k].append(attr_cur)
+            pm.append((split_index, coords, changed_triangles,
+                       new_triangles_opp_v, new_triangles_attr))
+            new_v_indices[rec.source] = num_v
+            num_v += 1
+            for tri_i in rec.deleted_triangles:
+                new_t_indices[tri_i] = num_t
+                num_t += 1
+        return pm
 
     def contractOnce(self):
         contr = self.nextContraction()
@@ -147,6 +226,9 @@ class MeshSimplification:
     def doContraction(self, contr):
         err, id, i1, i2, valid = contr
 
+        record = ContractionRecord(self.vertex_indices[i2],
+                                   self.vertex_indices[i1], [], [], [], [])
+
         for i in range(3):
             self.quadrics[i1][i] += self.quadrics[i2][i]
 
@@ -167,14 +249,28 @@ class MeshSimplification:
             # degenerate. Delete it.
             if tri_index in self.adj[i1]:
                 to_delete.append(tri_index)
+                record.deleted_triangles.append(self.triangle_indices[tri_index])
+                for i in (0,1,2):
+                    if self.triangles[tri_index][i] == i1:
+                        target = i
+                    elif self.triangles[tri_index][i] == i2:
+                        source = i
+                    else:
+                        opposite = i
+                record.deleted_triangles_opp_v.append(self.vertex_indices[
+                        self.triangles[tri_index][opposite]])
+                record.deleted_triangles_perm.append((source, target, opposite))
 
             # Otherwise, we need to generate some new contractions.
             else:
+                record.changed_triangles.append(self.triangle_indices[tri_index])
                 self.adj[i1][tri_index] = self.adj[i2][tri_index]
                 self.triangles[tri_index][self.adj[i1][tri_index]] = i1
                 for x in [(0,1),(0,2),(1,2)]:
                     self.genContraction(self.triangles[tri_index][x[0]],
                                         self.triangles[tri_index][x[1]])
+
+        self.history.append(record)
 
         to_delete.sort()
         to_delete.reverse()
@@ -194,6 +290,7 @@ class MeshSimplification:
         last = len(self.vertices) - 1
         if i2 == last:
             self.vertices.pop()
+            self.vertex_indices.pop()
             self.adj.pop()
             self.contractions.pop()
             self.quadrics.pop()
@@ -216,6 +313,7 @@ class MeshSimplification:
                 self.triangles[tri_index][self.adj[last][tri_index]] = i2
 
             self.vertices[i2] = self.vertices.pop()
+            self.vertex_indices[i2] = self.vertex_indices.pop()
             self.adj[i2] = self.adj.pop()
             self.contractions[i2] = self.contractions.pop()
             self.quadrics[i2] = self.quadrics.pop()
@@ -241,21 +339,82 @@ class MeshSimplification:
         for i in self.triangles[last]:
             del self.adj[i][last]
         self.triangles.pop()
-        for attr_list in self.corner_attributes:
-            attr_list.pop()
+        self.triangle_indices.pop()
+        for i in range(len(self.attributes)):
+            x = self.attributes[i].pop()
+            to_delete = []
+            for attr_i in x:
+                if last in self.attribute_adj[i][attr_i]:
+                    del self.attribute_adj[i][attr_i][last]
+                    if not self.attribute_adj[i][attr_i]:
+                        to_delete.append(attr_i)
+            
+            to_delete.sort()
+            to_delete.reverse()
+            for j in range(len(to_delete)):
+                self.swapAttributes(i, to_delete[j], len(self.attribute_sources[i]) - 1 - j)
+            for j in range(len(to_delete)):
+                self.deleteLastAttribute(i)
 
+                    
     def swapTriangles(self, i1, i2):
         if i1 == i2: return
 #        print "Swapping",i1,"(",self.triangles[i1],") with",i2,"(",self.triangles[i2],")"
         for i in set(list(self.triangles[i1]) + list(self.triangles[i2])):
             if i1 in self.adj[i] and i2 in self.adj[i]:
-                self.adj[i][i1], self.adj[i][i2] = self.adj[i][i2], self.adj[i][i1]
+                self.adj[i][i1], self.adj[i][i2] = \
+                    self.adj[i][i2], self.adj[i][i1]
             elif i1 in self.adj[i]:
                 self.adj[i][i2] = self.adj[i][i1]
                 del self.adj[i][i1]
             else:
                 self.adj[i][i1] = self.adj[i][i2]
                 del self.adj[i][i2]
-        self.triangles[i1], self.triangles[i2] = self.triangles[i2], self.triangles[i1]
-        for attr_list in self.corner_attributes:
+        for attr_id in range(len(self.attributes)):
+            for i in set(list(self.attributes[attr_id][i1]) +
+                         list(self.attributes[attr_id][i2])):
+                if i1 in self.attribute_adj[attr_id][i] and \
+                        i2 in self.attribute_adj[attr_id][i]:
+                    self.attribute_adj[attr_id][i][i1], \
+                        self.attribute_adj[attr_id][i][i2] = \
+                        self.attribute_adj[attr_id][i][i2], \
+                        self.attribute_adj[attr_id][i][i1]
+                elif i1 in self.attribute_adj[attr_id][i]:
+                    self.attribute_adj[attr_id][i][i2] = \
+                        self.attribute_adj[attr_id][i][i1]
+                    del self.attribute_adj[attr_id][i][i1]
+                else:
+                    self.attribute_adj[attr_id][i][i1] = \
+                        self.attribute_adj[attr_id][i][i2]
+                    del self.attribute_adj[attr_id][i][i2]
+        self.triangles[i1], self.triangles[i2] = \
+            self.triangles[i2], self.triangles[i1]
+        self.triangle_indices[i1], self.triangle_indices[i2] = \
+            self.triangle_indices[i2], self.triangle_indices[i1]
+        for attr_list in self.attributes:
             attr_list[i1], attr_list[i2] = attr_list[i2], attr_list[i1]
+
+    def swapAttributes(self, attr_id, i1, i2):
+        if i1 == i2: return
+        for i in set(list(self.attribute_adj[attr_id][i1]) +
+                     list(self.attribute_adj[attr_id][i2])):
+            for j in (0,1,2):
+                if self.attributes[attr_id][i][j] == i1:
+                    self.attributes[attr_id][i][j] = i2
+                elif self.attributes[attr_id][i][j] == i2:
+                    self.attributes[attr_id][i][j] = i1
+        self.attribute_adj[attr_id][i1], self.attribute_adj[attr_id][i2] = \
+            self.attribute_adj[attr_id][i2], self.attribute_adj[attr_id][i1]
+        self.attribute_sources[attr_id][i1], \
+            self.attribute_sources[attr_id][i2] = \
+            self.attribute_sources[attr_id][i2], \
+            self.attribute_sources[attr_id][i1]
+        self.attribute_indices[attr_id][i1], \
+            self.attribute_indices[attr_id][i2] = \
+            self.attribute_indices[attr_id][i2], \
+            self.attribute_indices[attr_id][i1]
+
+    def deleteLastAttribute(self, attr_id):
+        self.attribute_adj[attr_id].pop()
+        self.attribute_sources[attr_id].pop()
+        self.attribute_indices[attr_id].pop()
--- meshtool/filters/simplify_filters/simplify.py
+++ meshtool/filters/simplify_filters/simplify.py
@@ -18,8 +18,10 @@ def getSourceIdOrCreate(geom, source, new_id, components):
 def FilterGenerator():
     class ColladaSimplifyFilter(OpFilter):
         def __init__(self):
-            super(ColladaSimplifyFilter, self).__init__('simplify', 'Simplifies a collada file')
-            self.arguments.append(FileArgument('percent', 'Percentage of vertices to simplify to (0-100)'))
+            super(ColladaSimplifyFilter, self).__init__(
+                'simplify', 'Simplifies a collada file')
+            self.arguments.append(FilterArgument(
+                    'percent', 'Percentage of vertices to simplify to (0-100)'))
         def apply(self, mesh, percent):
             geom = mesh.geometries[0]
             prim = geom.primitives[0]
@@ -27,15 +29,22 @@ def FilterGenerator():
                 raise FilterException("Must triangulate before simplifying")
             percent = float(percent)
             if percent <= 0 or percent >= 100:
-                raise FilterException("Percent must be between 0 and 100, exclusive: "+str(percent))
+                raise FilterException(
+                    "Percent must be between 0 and 100, exclusive: " + 
+                    str(percent))
             print "Initializing..."
             if prim.texcoordset is not None:
-                corner_attributes = [indset for indset in prim.texcoord_indexset]
+                corner_attributes = [indset for indset
+                                     in prim.texcoord_indexset]
+                attribute_sources = [sl for sl in prim.texcoordset]
             else:
                 corner_attributes = []
+                attribute_sources = []
             if prim.normal is not None:
                 corner_attributes.append(prim.normal_index)
-            s = MeshSimplification(prim.vertex, prim.vertex_index, corner_attributes)
+                attribute_sources.append(prim.normal)
+            s = MeshSimplification(prim.vertex, prim.vertex_index,
+                                   corner_attributes, attribute_sources)
             num_contract = int(len(prim.vertex)*(100-percent)/100)
             print "Simplifying..."
             progress = ProgressPrinter(num_contract)
@@ -48,25 +57,40 @@ def FilterGenerator():
             new_id = geom.id+"-vertex"
             while new_id in geom.sourceById:
                 new_id += "-x"
-            vertexsource = collada.source.FloatSource(new_id, numpy.array(s.vertices), ('X', 'Y', 'Z'))
+            vertexsource = collada.source.FloatSource(
+                new_id, numpy.array(s.vertices), ('X', 'Y', 'Z'))
             geom.sourceById[new_id] = vertexsource
             offset = 0
             il.addInput(offset, "VERTEX", "#"+new_id)
             offset += 1
+            last = len(s.attributes) - 1
             if prim.normal is not None:
-                il.addInput(offset, "NORMAL", prim.sources['NORMAL'][0][2])
+                new_id = geom.id+"-normal"
+                while new_id in geom.sourceById:
+                    new_id += "-x"
+                normalsource = collada.source.FloatSource(
+                    new_id, numpy.array(s.attribute_sources[last]),
+                    ('X', 'Y', 'Z'))
+                geom.sourceById[new_id] = normalsource
+                il.addInput(offset, "NORMAL", "#"+new_id)
                 offset += 1
             if prim.texcoordset is not None:
                 texcoord_indexset = []
-                last = len(s.corner_attributes) - 1
                 for i in range(last):
-                    il.addInput(offset, "TEXCOORD", prim.sources['TEXCOORD'][i][2])
+                    new_id = geom.id+"-texcoord"+str(i)
+                    while new_id in geom.sourceById:
+                        new_id += "-x"
+                    texcoordsource = collada.source.FloatSource(
+                        new_id, numpy.array(s.attribute_sources[i]),
+                        ('S', 'T'))
+                    geom.sourceById[new_id] = texcoordsource
+                    il.addInput(offset, "TEXCOORD", "#"+new_id)
                     offset += 1
-                    texcoord_indexset.append(numpy.array(s.corner_attributes[i]))
+                    texcoord_indexset.append(numpy.array(s.attributes[i]))
             indices = numpy.array(s.triangles)
             indices.shape = (-1,3,1)
             if prim.normal is not None:
-                normal_index = numpy.array(s.corner_attributes[last])
+                normal_index = numpy.array(s.attributes[last])
                 normal_index.shape = (-1,3,1)
                 indices = numpy.append(indices, normal_index, 2)
             if prim.texcoordset is not None:
@@ -75,6 +99,19 @@ def FilterGenerator():
                     indices = numpy.append(indices, texcoord_index, 2)
             new_prim = geom.createTriangleSet(indices.flatten(), il, prim.material)
             geom.primitives[0] = new_prim
+
+            # Clean up unused sources
+            referenced_sources = {}
+            for prim in geom.primitives:
+                for semantic in prim.sources:
+                    for input in prim.sources[semantic]:
+                        referenced_sources[input[2][1:]] = True
+            unreferenced_sources = []
+            for id in geom.sourceById:
+                if id not in referenced_sources:
+                    unreferenced_sources.append(id)
+            for id in unreferenced_sources:
+                del geom.sourceById[id]
             print "Done."
             return mesh
     return ColladaSimplifyFilter()
