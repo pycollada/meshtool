ea389e94ddd935a51048f037ced6d02d28c399bb
Jeff Terrace <jterrace@gmail.com>
2011-05-03 18:44:54 -0400

    Bumping to 0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError: install_requires.append('argparse')
 
 setup(
     name = "meshtool",
-    version = "0.1.2",
+    version = "0.2",
     description = "Tool for manipulating collada meshes",
     author = "Jeff Terrace and contributors",
     author_email = 'jterrace@gmail.com',
