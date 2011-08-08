33a0271f083451f155060d74cc3728f5e7c25bb3
Jeff Terrace <jterrace@gmail.com>
2011-08-08 19:00:42 -0400

    Started optimization using texture stretch metric

--- meshtool/filters/simplify_filters/sander_simplify.py
+++ meshtool/filters/simplify_filters/sander_simplify.py
@@ -11,6 +11,7 @@ import heapq
 from render_utils import renderVerts, renderCharts
 from graph_utils import astar_path, dfs_interior_nodes, super_cycle
 import gc
+import random
 
 #python 2.5 set
 if not 'set' in __builtin__.__dict__:
@@ -71,11 +72,17 @@ def calcFitError(pts):
     
     return Efit
 
-def tri_surface_area(arr):
-    crosses = numpy.cross(arr[:,0] - arr[:,1], arr[:,0] - arr[:,2])
-    return numpy.sum(array_dot(crosses, crosses) / 2.0)
 
-def stretch_metric(t3d, t2d):
+def tri_areas_3d(arr):
+    crosses = numpy.cross(arr[:,0] - arr[:,1], arr[:,0] - arr[:,2])
+    return array_dot(crosses, crosses) / 2.0
+def tri_areas_2d(arr):
+    return ( arr[:,0,0]*(arr[:,1,1]-arr[:,2,1]) + 
+             arr[:,1,0]*(arr[:,2,1]-arr[:,0,1]) + 
+             arr[:,2,0]*(arr[:,0,1]-arr[:,1,1])
+            ) / 2.0
+
+def stretch_metric(t3d, t2d, return_A2d=False, flippedCheck=None):
     q1 = t3d[:,0]
     q2 = t3d[:,1]
     q3 = t3d[:,2]
@@ -85,15 +92,21 @@ def stretch_metric(t3d, t2d):
     t1 = t2d[:,0,1]
     t2 = t2d[:,1,1]
     t3 = t2d[:,2,1]
-    A = ((s2-s1)*(t3-t1) - (s3-s1)*(t2-t1)) / 2.0
+    A2d = ((s2-s1)*(t3-t1) - (s3-s1)*(t2-t1)) / 2.0
     
-    S_s = (q1*(t2-t3)[:,None] + q2*(t3-t1)[:,None] + q3*(t1-t2)[:,None]) / (2.0 * A)[:,None]
-    S_t = (q1*(s3-s2)[:,None] + q2*(s1-s3)[:,None] + q3*(s2-s1)[:,None]) / (2.0 * A)[:,None]
+    S_s = (q1*(t2-t3)[:,None] + q2*(t3-t1)[:,None] + q3*(t1-t2)[:,None]) / (2.0 * A2d)[:,None]
+    S_t = (q1*(s3-s2)[:,None] + q2*(s1-s3)[:,None] + q3*(s2-s1)[:,None]) / (2.0 * A2d)[:,None]
     
-    print S_s
-    print S_t
-    
-    #L2_1 = math.sqrt((numpy.dot(S_s,S_s) + numpy.dot(S_t,S_t)) / 2.0)
+    L2 = numpy.sqrt((array_mult(S_s,S_s) + array_mult(S_t,S_t)) / 2.0)
+    if flippedCheck is not None:
+        L2[numpy.logical_not(numpy.logical_xor(A2d < 0, flippedCheck < 0))] = numpy.inf
+    #L2[A2d < 0] = numpy.Inf
+    #if normalize:
+    #    A3d = tri_areas_3d(t3d)
+    #    L2 = numpy.sqrt(numpy.sum(L2*L2*A3d) / numpy.sum(A3d)) * numpy.sqrt(numpy.sum(A2d) / numpy.sum(A3d))
+    if return_A2d:
+        return L2, A2d
+    return L2
 
 def begin_operation():
     gc.disable()
@@ -534,7 +547,7 @@ def sandler_simplify(mesh):
         for edge in boundary_path:
             angle = v3dist(all_vertices[edge[0]], all_vertices[edge[1]]) / total_dist
             curangle += angle * 2 * math.pi
-            x, y = math.sin(curangle), math.cos(curangle)
+            x, y = (math.sin(curangle) + 1) / 2.0, (math.cos(curangle) + 1.0) / 2.0
             vertexgraph.add_node(edge[0], u=x)
             vertexgraph.add_node(edge[0], v=y)
         
@@ -581,7 +594,7 @@ def sandler_simplify(mesh):
             interior_us = numpy.linalg.solve(A, Bu)
             interior_vs = numpy.linalg.solve(A, Bv)
             for (i, (u, v)) in enumerate(zip(interior_us, interior_vs)):
-                vertexgraph.add_node(interior_verts[i], u=u, v=v, stretch=0)
+                vertexgraph.add_node(interior_verts[i], u=u[0], v=v[0], stretch=0)
         
         #import Image, ImageDraw
         #W, H = 500, 500
@@ -594,8 +607,8 @@ def sandler_simplify(mesh):
         #    u2 = vertexgraph.node[pt2]['u']
         #    v1 = vertexgraph.node[pt1]['v']
         #    v2 = vertexgraph.node[pt2]['v']
-        #    uv1 = ( (u1+1)/2.0 * W, (v1+1)/2.0 * H )
-        #    uv2 = ( (u2+1)/2.0 * W, (v2+1)/2.0 * H )
+        #    uv1 = ( u1 * W, v1 * H )
+        #    uv2 = ( u2 * W, v2 * H )
         #    draw.ellipse((uv1[0]-5, uv1[1]-5, uv1[0]+5, uv1[1]+5), outline=(0,0,0), fill=(255,0,0))
         #    draw.ellipse((uv2[0]-5, uv2[1]-5, uv2[0]+5, uv2[1]+5), outline=(0,0,0), fill=(255,0,0))
         #    draw.line([uv1, uv2], fill=(0,0,0))
@@ -614,41 +627,81 @@ def sandler_simplify(mesh):
     begin_operation()
     for (face, facedata) in facegraph.nodes_iter(data=True):
         border_edges = facedata['edges']
-        chart_tris = facedata['tris']
-        tri_3d = all_vertices[numpy.array(chart_tris)]
+        chart_tris = numpy.array(facedata['tris'])
+        tri_3d = all_vertices[chart_tris]
         tri_2d = numpy.array([[
                    [vertexgraph.node[tri[0]]['u'], vertexgraph.node[tri[0]]['v']],
                    [vertexgraph.node[tri[1]]['u'], vertexgraph.node[tri[1]]['v']],
                    [vertexgraph.node[tri[2]]['u'], vertexgraph.node[tri[2]]['v']]]
                   for tri in chart_tris ])
         
+        #TODO: REMOVEME
         if len(chart_tris) < 2:
             continue
         
-        tri_3d1 = tri_3d[0]
-        tri_2d1 = tri_2d[0]
+        L2 = stretch_metric(tri_3d, tri_2d)
         
-        q1 = tri_3d1[0]
-        q2 = tri_3d1[1]
-        q3 = tri_3d1[2]
-        s1 = tri_2d1[0][0]
-        s2 = tri_2d1[1][0]
-        s3 = tri_2d1[2][0]
-        t1 = tri_2d1[0][1]
-        t2 = tri_2d1[1][1]
-        t3 = tri_2d1[2][1]
-        A = ((s2-s1)*(t3-t1) - (s3-s1)*(t2-t1)) / 2.0
-        S_s = (q1*(t2-t3) + q2*(t3-t1) + q3*(t1-t2)) / (2.0 * A)
-        S_t = (q1*(s3-s2) + q2*(s1-s3) + q3*(s2-s1)) / (2.0 * A)
+        unique_verts, index_map = numpy.unique(chart_tris, return_inverse=True)
+        index_map.shape = chart_tris.shape
+        neighborhood_stretch = numpy.zeros(unique_verts.shape, dtype=numpy.float32)
+
+        neighborhood_stretch[index_map[:,0]] += L2
+        neighborhood_stretch[index_map[:,1]] += L2
+        neighborhood_stretch[index_map[:,2]] += L2
+
+        vert_stretch_heap = zip(-1 * neighborhood_stretch, unique_verts)
+        heapq.heapify(vert_stretch_heap)
         
-        L2_1 = math.sqrt((numpy.dot(S_s,S_s) + numpy.dot(S_t,S_t)) / 2.0)
-        print L2_1
-        
-        print stretch_metric(tri_3d, tri_2d)
-        
-        import sys
-        sys.exit(0)
-        
+        while len(vert_stretch_heap) > 0:
+            stretch, vert = heapq.heappop(vert_stretch_heap)
+            print vert, stretch
+            
+            ucoord, vcoord = vertexgraph.node[vert]['u'], vertexgraph.node[vert]['v']
+            
+            adj_tris = chart_tris[numpy.logical_or(numpy.logical_or(
+                            chart_tris[:,0] == vert,
+                            chart_tris[:,1] == vert),
+                            chart_tris[:,2] == vert)]
+            neighborhood_tri3d = all_vertices[adj_tris]
+            neighborhood_tri_2d = numpy.array([[
+                       [vertexgraph.node[tri[0]]['u'], vertexgraph.node[tri[0]]['v']],
+                       [vertexgraph.node[tri[1]]['u'], vertexgraph.node[tri[1]]['v']],
+                       [vertexgraph.node[tri[2]]['u'], vertexgraph.node[tri[2]]['v']]]
+                      for tri in adj_tris ])
+            neighborhood_L2, origA2d = stretch_metric(neighborhood_tri3d, neighborhood_tri_2d, return_A2d=True)
+            print 'orig L2', neighborhood_L2
+            
+            randangle = random.uniform(0, 2 * math.pi)
+            randslope = math.tan(randangle)
+
+            # y - y1 = m(x - x1)
+            def yfromx(x):
+                return randslope * (x - ucoord) + vcoord
+            def xfromy(y):
+                return ((y - vcoord) / randslope) + ucoord
+            
+            xintercept0 = yfromx(0)
+            minx = 0 if 0 < xintercept0 < 1 else xfromy(1)
+            xintercept1 = yfromx(1)
+            maxx = 1 if 0 < xintercept1 < 1 else xfromy(0)
+            minx, maxx = tuple(sorted([minx, maxx]))
+            
+            samples = 10.0
+            step = (maxx - minx) / samples
+            for xval in numpy.arange(minx, maxx, step):
+                yval = yfromx(xval)
+                print 'x,y', xval, yval
+                vertexgraph.add_node(vert, u=xval, v=yval)
+                neighborhood_tri_2d = numpy.array([[
+                           [vertexgraph.node[tri[0]]['u'], vertexgraph.node[tri[0]]['v']],
+                           [vertexgraph.node[tri[1]]['u'], vertexgraph.node[tri[1]]['v']],
+                           [vertexgraph.node[tri[2]]['u'], vertexgraph.node[tri[2]]['v']]]
+                          for tri in adj_tris ])
+                neighborhood_L2 = stretch_metric(neighborhood_tri3d, neighborhood_tri_2d, flippedCheck=origA2d)
+                print 'trying L2', neighborhood_L2
+            
+            import sys
+            sys.exit(0)
         
     end_operation()
     print next(t)
