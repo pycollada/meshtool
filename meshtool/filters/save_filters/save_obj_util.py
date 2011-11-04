60c5931804ac4b7cd52639d7c29c3c794f0f9b85
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2011-11-03 22:40:24 -0700

    Make OBJ saving handle empty (but non-None) texcoordsets.

--- meshtool/filters/save_filters/save_obj_util.py
+++ meshtool/filters/save_filters/save_obj_util.py
@@ -59,19 +59,24 @@ def write_obj(mesh, mtlfilename, f):
     for boundgeom in mesh.scene.objects('geometry'):
         print >>f, "# %s - %s" % (boundgeom.original.name, boundgeom.original.id)
         for boundprim in boundgeom.primitives():
+            # Determine the properties of these primitives we're going
+            # to use
+            emit_normals = boundprim.normal is not None
+            emit_texcoords = boundprim.texcoordset is not None and len(boundprim.texcoordset) > 0
+            if emit_texcoords and len(boundprim.texcoordset) > 1:
+                raise FilterException("OBJ only supports one texture coordinate set.")
+
             # Write transformed vertices, normals, texcoords
             for vi in range(boundprim.vertex.shape[0]):
                 v = boundprim.vertex[vi,:]
                 print >>f, "v %f %f %f" % (v[0], v[1], v[2])
 
-            if boundprim.normal is not None:
+            if emit_normals:
                 for vi in range(boundprim.normal.shape[0]):
                     v = boundprim.normal[vi,:]
                     print >>f, "vn %f %f %f" % (v[0], v[1], v[2])
 
-            if boundprim.texcoordset is not None:
-                if len(boundprim.texcoordset) > 1:
-                    raise FilterException("OBJ only supports one texture coordinate set.")
+            if emit_texcoords:
                 for vi in range(boundprim.texcoordset[0].shape[0]):
                     v = boundprim.texcoordset[0][vi,:]
                     print >>f, "vt %f %f" % (v[0], v[1])
@@ -82,10 +87,10 @@ def write_obj(mesh, mtlfilename, f):
             # Write transformed primitives
             for face in boundprim:
                 print >>f, "f ",
-                if face.indices is not None and face.normal_indices is not None and face.texcoord_indices is not None:
+                if emit_normals and emit_texcoords:
                     for vidx,nidx,tcidx in zip(face.indices,face.normal_indices,face.texcoord_indices[0]):
                         print >>f, "%d/%d/%d " % (vidx + vert_offset, tcidx + tc_offset, nidx + norm_offset),
-                elif face.indices is not None and face.normal_indices is not None:
+                elif emit_normals:
                     for vidx,nidx in zip(face.indices,face.normal_indices):
                         print >>f, "%d//%d " % (vidx + vert_offset, nidx + norm_offset),
                 else:
@@ -95,7 +100,7 @@ def write_obj(mesh, mtlfilename, f):
 
             # Finally, update offsets
             vert_offset += boundprim.vertex.shape[0]
-            if boundprim.normal is not None:
+            if emit_normals:
                 norm_offset += boundprim.normal.shape[0]
-            if boundprim.texcoordset is not None:
+            if emit_texcoords:
                 tc_offset += boundprim.texcoordset[0].shape[0]
