131cb610b18a372beddbc88ab8a1b672492ebee6
Jeff Terrace <jterrace@gmail.com>
2011-11-06 12:23:45 -0500

    Change the way filters are imported so any module that fails to import a dependency is ommitted

--- meshtool/filters/__init__.py
+++ meshtool/filters/__init__.py
@@ -4,61 +4,89 @@ import sys
 factory = FilterFactory()
 
 #Load filters first
-import meshtool.filters.load_filters.load_collada
+try: import meshtool.filters.load_filters.load_collada
+except ImportError: pass
 
 #Print filters
-import meshtool.filters.print_filters.print_textures
-import meshtool.filters.print_filters.print_json
-import meshtool.filters.print_filters.print_info
-import meshtool.filters.print_filters.print_instances
-import meshtool.filters.print_filters.print_scene
-import meshtool.filters.print_filters.print_render_info
+try: import meshtool.filters.print_filters.print_textures
+except ImportError: pass
+try: import meshtool.filters.print_filters.print_json
+except ImportError: pass
+try: import meshtool.filters.print_filters.print_info
+except ImportError: pass
+try: import meshtool.filters.print_filters.print_instances
+except ImportError: pass
+try: import meshtool.filters.print_filters.print_scene
+except ImportError: pass
+try: import meshtool.filters.print_filters.print_render_info
+except ImportError: pass
 
 #Viewer
-try:
-    import meshtool.filters.panda_filters.viewer
-    import meshtool.filters.panda_filters.collada_viewer
-    import meshtool.filters.panda_filters.pm_viewer
-except:
-    pass
+try: import meshtool.filters.panda_filters.viewer
+except ImportError: pass
+try: import meshtool.filters.panda_filters.collada_viewer
+except ImportError: pass
+try: import meshtool.filters.panda_filters.pm_viewer
+except ImportError: pass
 
 #Optimizations
-import meshtool.filters.optimize_filters.combine_effects
-import meshtool.filters.optimize_filters.combine_materials
-import meshtool.filters.optimize_filters.combine_primitives
-import meshtool.filters.optimize_filters.strip_lines
-import meshtool.filters.optimize_filters.strip_empty_geometry
-import meshtool.filters.optimize_filters.strip_unused_sources
-import meshtool.filters.optimize_filters.triangulate
-import meshtool.filters.optimize_filters.generate_normals
-import meshtool.filters.optimize_filters.save_mipmaps
-import meshtool.filters.optimize_filters.optimize_textures
-import meshtool.filters.optimize_filters.adjust_texcoords
-import meshtool.filters.optimize_filters.normalize_indices
-import meshtool.filters.optimize_filters.split_triangle_texcoords
-import meshtool.filters.optimize_filters.optimize_sources
+try: import meshtool.filters.optimize_filters.combine_effects
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.combine_materials
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.combine_primitives
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.strip_lines
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.strip_empty_geometry
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.strip_unused_sources
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.triangulate
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.generate_normals
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.save_mipmaps
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.optimize_textures
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.adjust_texcoords
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.normalize_indices
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.split_triangle_texcoords
+except ImportError: pass
+try: import meshtool.filters.optimize_filters.optimize_sources
+except ImportError: pass
 
 #Atlasing
-import meshtool.filters.atlas_filters.make_atlases
+try: import meshtool.filters.atlas_filters.make_atlases
+except ImportError: pass
 
 #Simplification
-import meshtool.filters.simplify_filters.simplify
-import meshtool.filters.simplify_filters.load_pm
-import meshtool.filters.simplify_filters.sander_simplify
+try: import meshtool.filters.simplify_filters.sander_simplify
+except ImportError: pass
 
 #Meta filters
-import meshtool.filters.meta_filters.medium_optimizations
-import meshtool.filters.meta_filters.full_optimizations
+try: import meshtool.filters.meta_filters.medium_optimizations
+except ImportError: pass
+try: import meshtool.filters.meta_filters.full_optimizations
+except ImportError: pass
 
 #Save filters last
-try:
-    import meshtool.filters.panda_filters.save_screenshot
-    import meshtool.filters.panda_filters.save_rotate_screenshots
-except:
-    pass
-import meshtool.filters.save_filters.save_collada
-import meshtool.filters.save_filters.save_collada_zip
-import meshtool.filters.save_filters.save_badgerfish
-import meshtool.filters.save_filters.save_ply
-import meshtool.filters.save_filters.save_obj
-import meshtool.filters.save_filters.save_obj_zip
+try: import meshtool.filters.panda_filters.save_screenshot
+except ImportError: pass
+try: import meshtool.filters.panda_filters.save_rotate_screenshots
+except ImportError: pass
+try: import meshtool.filters.save_filters.save_collada
+except ImportError: pass
+try: import meshtool.filters.save_filters.save_collada_zip
+except ImportError: pass
+try: import meshtool.filters.save_filters.save_badgerfish
+except ImportError: pass
+try: import meshtool.filters.save_filters.save_ply
+except ImportError: pass
+try: import meshtool.filters.save_filters.save_obj
+except ImportError: pass
+try: import meshtool.filters.save_filters.save_obj_zip
+except ImportError: pass
