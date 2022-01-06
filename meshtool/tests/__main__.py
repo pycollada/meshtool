1a72ecb223284cac41a026706b3ca75b75564189
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-06 23:16:35 +0800

    Delete meshtool directory

--- meshtool/tests/__main__.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import sys
-
-if __name__ == '__main__':
-    suite = unittest.TestLoader().discover("tests")
-    ret = unittest.TextTestRunner(verbosity=2).run(suite)
-    if ret.wasSuccessful():
-        sys.exit(0)
-    sys.exit(1)
